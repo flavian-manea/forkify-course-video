2bc7b4dccb75a5870d620a94d5ebadb2344cd736
Flavian Manea <flavian@storii.com>
2022-03-18 20:13:12 +0200

    Added console message

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -116,6 +116,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the new application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -124,6 +128,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 
 init();
