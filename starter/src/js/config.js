fdd6f40ed8a14c637fb2cf9a03f3b4999702f1c3
Flavian Manea <flavian@storii.com>
2022-03-18 19:43:09 +0200

    initial commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f4387953-458b-40ba-b2e1-27567518a8d8';
+export const MODAL_CLOSE_SEC = 2.5;
